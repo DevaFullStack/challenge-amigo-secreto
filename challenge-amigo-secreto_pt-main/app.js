768d6ea2a5f878fa950c57bd8d0d9159bc496fdc
DevaFullStack <amelia.ceo@gmail.com>
2025-03-08 19:55:21 -0300

    Criar função adicionar amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nomeDosAmigos = document.getElementById('amigo').value;
+    console.log(nomeDosAmigos);
+}
