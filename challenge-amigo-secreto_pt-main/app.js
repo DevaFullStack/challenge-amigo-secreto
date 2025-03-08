8573918e7e8eb7d53a6c4fbf9eb9950fc41bd80a
DevaFullStack <amelia.ceo@gmail.com>
2025-03-08 20:10:52 -0300

    Adicionar nome na lista de amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,7 +1,8 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigos = [];
+let ListaDeAmigos = [];
 
 function adicionarAmigo() {
     let nomeDosAmigos = document.getElementById('amigo').value;
-    console.log(nomeDosAmigos);
+    ListaDeAmigos.push(nomeDosAmigos);
+    console.log(ListaDeAmigos);
 }
