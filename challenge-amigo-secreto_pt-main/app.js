f193e10789829656cd611e5c2de8aeed809932e5
DevaFullStack <amelia.ceo@gmail.com>
2025-03-11 16:30:51 -0300

    Verificar preenchimento do campo amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,8 +1,12 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let ListaDeAmigos = [];
 
-function adicionarAmigo() {
+function adicionarAmigos() {
     let nomeDosAmigos = document.getElementById('amigo').value;
-    ListaDeAmigos.push(nomeDosAmigos);
-    console.log(ListaDeAmigos);
+    if (nomeDosAmigos == '') {
+        alert('Por favor, insira um nome');
+    } else {
+        ListaDeAmigos.push(nomeDosAmigos);
+        alert('Amigo adicionado com sucesso!');
+    }
 }
