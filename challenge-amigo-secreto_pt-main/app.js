8c87342709b0fea6f657381432e8611a12c2495a
DevaFullStack <amelia.ceo@gmail.com>
2025-03-11 16:56:08 -0300

    Criar função limpar campo

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,12 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let ListaDeAmigos = [];
 
-function adicionarAmigos() {
+function adicionarAmigo() {
     let nomeDosAmigos = document.getElementById('amigo').value;
     if (nomeDosAmigos == '') {
         alert('Por favor, insira um nome');
     } else {
         ListaDeAmigos.push(nomeDosAmigos);
         alert('Amigo adicionado com sucesso!');
+        limparCampo();
     }
+}
+
+function limparCampo() {
+    nomeDosAmigos = document.querySelector('input').value = '';
 }
